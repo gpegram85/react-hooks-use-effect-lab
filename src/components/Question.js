ceeb75018145815f523158de0ae558bf43d380cb
Grey Pegram <dpegram85@gmail.com>
2024-05-25 20:24:37 -0400

    Passed npm tests, doesn't work right in the browser. If fixed for browser, doesn't pass tests

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,28 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect( () => {
+    setTimeRemaining(10)
+      const timeout = setTimeout( () => {
+        const interval = setInterval( () => {
+          setTimeRemaining(timeRemaining => {
+            if (timeRemaining > 1) {
+              return timeRemaining - 1
+            } else {
+              onAnswered(false)
+              clearInterval(interval)
+              return 10
+            }
+          })
+        }, 1000)
+
+          return () => clearInterval(interval)
+        }, 0)
+
+          return () => clearTimeout(timeout)
+        }, [onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
-    onAnswered(isCorrect);
+    setTimeout(() => onAnswered(isCorrect), 0)
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+  const { id, prompt, answers, correctIndex } = question
 
   return (
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
+        const isCorrect = index === correctIndex
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
           </button>
-        );
+        )
       })}
       <h5>{timeRemaining} seconds remaining</h5>
     </>
